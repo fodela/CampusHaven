ab4b4dfcc8c61d2da363cef796bb6d6735732b12
fodela <particleme30@gmail.com>
2022-07-23 19:59:11 +0000

    installed and configured tailwind

---
+++ frontend/src/App.js
@@ -0,0 +1,4 @@
+function App() {
+	return <div>Welcome to CampusHaven</div>;
+}
+export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#136a35",
+				secondary: "#B4D4C6",
+			},
+		},
+	},
+	plugins: [],
+};
